3caf508c4a2f65f4e793a1ec96fd9c12f8d789ff
sholto <arjunc96@gmail.com>
2026-04-07 10:30:45 +0530

    Create condition.sol

---
+++ condition.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.20;
+
+contract Conditions{
+function fun1(uint256 var1) public returns (string memory){
+//var1 >= 90 returns excellent
+// var1 >= 50 returns pass
+//else return fail
+
+if(var1 >= 90){
+return "Excellent";
+}
+else if(var1 >= 50){
+return "Pass";
+}
+else{
+return "Fail";
+}
+}
+}
