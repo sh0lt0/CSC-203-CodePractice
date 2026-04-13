3e7fcde78d719f3195df303e72f73d4449dc2c67
sholto <arjunc96@gmail.com>
2026-04-13 10:09:22 +0530

    Update visibilty-fun.sol

--- visibilty-fun.sol
+++ visibilty-fun.sol
@@ -2,18 +2,40 @@ pragma solidity ^0.8.0;
 
 contract C1 {
 
+    uint256 public x = 2;
+
     function fun1() public  {}
-    function fun2()public {
+    function fun2() external  {
 
         fun1();
     }
+    
+    function fun3() public {
+       // fun2();  // gives error as fun2() is external
+    }
+    function fun4() internal returns(uint){
+        return x;  // gives error as fun2() is external
+    }
+
     }
 
 contract C2 {
 
-    C1 c1 = new C1();
+    C1 c1 = new C1(); // deploying an instance of C1
     
    function callFun1() public {
     c1.fun1();
     }
+
+    function callFun2() public {
+    c1.fun2();
+    }
+}
+
+contract child1 is C1{ // declaring child1 as child contract of C1
+
+   function callFun4() public {
+    fun4();
+    }
+
 }
